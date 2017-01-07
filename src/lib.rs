a8b50423c4586acf6200521b79fcd5ac294c302b
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-07 05:17:33 +0200

    New Object trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+/// Object is an instance of some program that lives in computer
+/// memory and may perform some tasks.
+pub trait Object {
+
+    /// Type that is uniquely identifying objects in the system.
+    type ObjectId;
+
+    /// Get object unique identifier.
+    fn id(&self) -> Self::ObjectId;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
