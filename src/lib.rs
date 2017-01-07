469f187e669132bacc04b5aafa7cc487ab4ad94b
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-07 09:19:15 +0200

    New Master trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+/// Holds basic functions for creating channels, services, object etc. in
+/// the system. Each CCS model implementation has it's own Master. Thus,
+/// each OS architecture implement their specific Master.
+pub trait Master {
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
