4ffb5a805c97c9f73c66b98a8f63dcac9a4bfe0d
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-07 09:23:41 +0200

    New Object trait

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@
 pub trait Master {
 }
 
+/// Object is sort of process in Kobzar. It is an instanse of some
+/// program that is currently running on the system, or residing in
+/// the RAM. The CCS object may request services of other objects which
+/// either already are loaded or can be loaded to reply on the request.
+pub trait Object {
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
