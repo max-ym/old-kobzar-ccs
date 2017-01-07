cc45078ee70d477123aa3463ab13dca5b163fe46
Maxym Naumchyk <max.naumch@gmail.com>
2017-01-07 06:36:27 +0200

    Reverted changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,3 @@
-/// Object is an instance of some program that lives in computer
-/// memory and may perform some tasks.
-pub trait Object {
-
-    /// Type that is uniquely identifying objects in the system.
-    type Id;
-
-    /// Get object unique identifier.
-    fn id(&self) -> Self::Id;
-}
-
-pub trait Service  {
-
-    /// Type that is uniquely identifying services in the system.
-    type Id;
-
-    /// Get service identifier.
-    fn id(&self) -> Self::Id;
-}
-
-pub trait Channel<ServiceType, ObjectType> where
-        ServiceType : Service,
-        ObjectType  : Object {
-
-    /// Get requester-object reference.
-    fn requester(&self) -> &ObjectType;
-
-    /// Get requested service reference.
-    fn service(&self) -> &ServiceType;
-}
-
-pub trait Page {
-    // Empty currently.
-}
-
 #[cfg(test)]
 mod tests {
     #[test]
